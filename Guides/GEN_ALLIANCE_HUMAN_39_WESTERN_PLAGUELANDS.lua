c62f9bcf94e90970ad3678942b5682124c74ce95
prezus <prezus@gmail.com>
2026-09-25 15:20:17 -0600

    Guides: 39. Western Plaguelands 52-52 (Human)

--- Guides/GEN_ALLIANCE_HUMAN_39_WESTERN_PLAGUELANDS.lua
+++ Guides/GEN_ALLIANCE_HUMAN_39_WESTERN_PLAGUELANDS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_HUMAN_39_WESTERN_PLAGUELANDS",
     name = "39. Western Plaguelands 52-52 (Human)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Human" },
     minLevel = 52,
