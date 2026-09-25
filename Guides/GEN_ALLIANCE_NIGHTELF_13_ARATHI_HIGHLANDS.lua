d2ada4a526a94e9227f5e50e5f84b0c2c2f8b1d8
prezus <prezus@gmail.com>
2026-09-25 15:20:19 -0600

    Guides: 13. Arathi Highlands 30-31 (Night Elf)

--- Guides/GEN_ALLIANCE_NIGHTELF_13_ARATHI_HIGHLANDS.lua
+++ Guides/GEN_ALLIANCE_NIGHTELF_13_ARATHI_HIGHLANDS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_NIGHTELF_13_ARATHI_HIGHLANDS",
     name = "13. Arathi Highlands 30-31 (Night Elf)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "NightElf" },
     minLevel = 30,
