ddc95912c494e1058fa9a1df5ba0bede1d05dd64
prezus <prezus@gmail.com>
2026-09-25 15:20:30 -0600

    Guides: 33. Burning Steppes 50-51 (Skyborne)

--- Guides/GEN_HORDE_SKYBORNE_33_BURNING_STEPPES.lua
+++ Guides/GEN_HORDE_SKYBORNE_33_BURNING_STEPPES.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_HORDE_SKYBORNE_33_BURNING_STEPPES",
     name = "33. Burning Steppes 50-51 (Skyborne)",
-    version = 2,
+    version = 3,
     faction = "Horde",
     race = { "Skyborne" },
     minLevel = 50,
