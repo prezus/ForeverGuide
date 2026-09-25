115da6e7c9bd02ee95278ca5a79e138489f4540b
prezus <prezus@gmail.com>
2026-09-25 15:20:16 -0600

    Guides: 22. The Hinterlands 42-45 (Human)

--- Guides/GEN_ALLIANCE_HUMAN_22_THE_HINTERLANDS.lua
+++ Guides/GEN_ALLIANCE_HUMAN_22_THE_HINTERLANDS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_HUMAN_22_THE_HINTERLANDS",
     name = "22. The Hinterlands 42-45 (Human)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Human" },
     minLevel = 42,
