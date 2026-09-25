00d0c330516e54dcc886238e71b8eb1aabc9e056
prezus <prezus@gmail.com>
2026-09-25 15:20:22 -0600

    Guides: 21. Felwood 45-45 (Skyborne)

--- Guides/GEN_ALLIANCE_SKYBORNE_21_FELWOOD.lua
+++ Guides/GEN_ALLIANCE_SKYBORNE_21_FELWOOD.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_SKYBORNE_21_FELWOOD",
     name = "21. Felwood 45-45 (Skyborne)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Skyborne" },
     minLevel = 45,
