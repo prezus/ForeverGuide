be728b324cc7a7b10ecbbc54c1151235d8b77d2a
prezus <prezus@gmail.com>
2026-09-25 15:20:14 -0600

    Guides: 43. Winterspring 53-53 (Dwarf)

--- Guides/GEN_ALLIANCE_DWARF_43_WINTERSPRING.lua
+++ Guides/GEN_ALLIANCE_DWARF_43_WINTERSPRING.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_DWARF_43_WINTERSPRING",
     name = "43. Winterspring 53-53 (Dwarf)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Dwarf", "Gnome" },
     minLevel = 53,
