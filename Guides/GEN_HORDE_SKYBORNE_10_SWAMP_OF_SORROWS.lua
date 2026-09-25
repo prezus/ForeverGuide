563596e67c043d771bdb9257007a73c73bc4a4cd
prezus <prezus@gmail.com>
2026-09-25 15:20:29 -0600

    Guides: 10. Swamp of Sorrows 38-38 (Skyborne)

--- Guides/GEN_HORDE_SKYBORNE_10_SWAMP_OF_SORROWS.lua
+++ Guides/GEN_HORDE_SKYBORNE_10_SWAMP_OF_SORROWS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_HORDE_SKYBORNE_10_SWAMP_OF_SORROWS",
     name = "10. Swamp of Sorrows 38-38 (Skyborne)",
-    version = 2,
+    version = 3,
     faction = "Horde",
     race = { "Skyborne" },
     minLevel = 38,
