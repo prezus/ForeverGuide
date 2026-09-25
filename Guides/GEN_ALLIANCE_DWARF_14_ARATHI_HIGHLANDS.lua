a8c0f55b90c8858737916fa9a4cc4896a88e257e
prezus <prezus@gmail.com>
2026-09-25 15:20:12 -0600

    Guides: 14. Arathi Highlands 33-33 (Dwarf)

--- Guides/GEN_ALLIANCE_DWARF_14_ARATHI_HIGHLANDS.lua
+++ Guides/GEN_ALLIANCE_DWARF_14_ARATHI_HIGHLANDS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_DWARF_14_ARATHI_HIGHLANDS",
     name = "14. Arathi Highlands 33-33 (Dwarf)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Dwarf", "Gnome" },
     minLevel = 33,
