9d3427399cacaca0f25c52c16d333588e3d50322
prezus <prezus@gmail.com>
2026-09-25 15:20:29 -0600

    Guides: 11. Dustwallow Marsh 38-40 (Skyborne)

--- Guides/GEN_HORDE_SKYBORNE_11_DUSTWALLOW_MARSH.lua
+++ Guides/GEN_HORDE_SKYBORNE_11_DUSTWALLOW_MARSH.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_HORDE_SKYBORNE_11_DUSTWALLOW_MARSH",
     name = "11. Dustwallow Marsh 38-40 (Skyborne)",
-    version = 2,
+    version = 3,
     faction = "Horde",
     race = { "Skyborne" },
     minLevel = 38,
