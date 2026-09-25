63668c27b2646e40a8443125419aead8782fef7e
prezus <prezus@gmail.com>
2026-09-25 15:20:24 -0600

    Guides: 18. Searing Gorge 45-45 (Orc)

--- Guides/GEN_HORDE_ORC_18_SEARING_GORGE.lua
+++ Guides/GEN_HORDE_ORC_18_SEARING_GORGE.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_HORDE_ORC_18_SEARING_GORGE",
     name = "18. Searing Gorge 45-45 (Orc)",
-    version = 2,
+    version = 3,
     faction = "Horde",
     race = { "Orc", "Troll" },
     minLevel = 45,
