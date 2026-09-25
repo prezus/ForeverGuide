8ac0df739ea4db97e111883584fe9aa541a32448
prezus <prezus@gmail.com>
2026-09-25 15:20:22 -0600

    Guides: 18. Feralas 40-41 (Skyborne)

--- Guides/GEN_ALLIANCE_SKYBORNE_18_FERALAS.lua
+++ Guides/GEN_ALLIANCE_SKYBORNE_18_FERALAS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_SKYBORNE_18_FERALAS",
     name = "18. Feralas 40-41 (Skyborne)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Skyborne" },
     minLevel = 40,
