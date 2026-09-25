b0f4478eedfc2e3b83526eb1022cc2da6f35f706
prezus <prezus@gmail.com>
2026-09-25 15:20:20 -0600

    Guides: 25. Searing Gorge 46-47 (Night Elf)

--- Guides/GEN_ALLIANCE_NIGHTELF_25_SEARING_GORGE.lua
+++ Guides/GEN_ALLIANCE_NIGHTELF_25_SEARING_GORGE.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_NIGHTELF_25_SEARING_GORGE",
     name = "25. Searing Gorge 46-47 (Night Elf)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "NightElf" },
     minLevel = 46,
