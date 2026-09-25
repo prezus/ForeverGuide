7cafb5d242ddf9da6d226bd518eec5676b2c7175
prezus <prezus@gmail.com>
2026-09-25 15:20:17 -0600

    Guides: 27. Feralas 47-47 (Human)

--- Guides/GEN_ALLIANCE_HUMAN_27_FERALAS.lua
+++ Guides/GEN_ALLIANCE_HUMAN_27_FERALAS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_HUMAN_27_FERALAS",
     name = "27. Feralas 47-47 (Human)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Human" },
     minLevel = 47,
