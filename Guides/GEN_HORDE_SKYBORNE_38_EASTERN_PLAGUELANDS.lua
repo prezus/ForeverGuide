5765a4517de08ec122f2722bfbd37f0d8563959f
prezus <prezus@gmail.com>
2026-09-25 15:20:31 -0600

    Guides: 38. Eastern Plaguelands 52-53 (Skyborne)

--- Guides/GEN_HORDE_SKYBORNE_38_EASTERN_PLAGUELANDS.lua
+++ Guides/GEN_HORDE_SKYBORNE_38_EASTERN_PLAGUELANDS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_HORDE_SKYBORNE_38_EASTERN_PLAGUELANDS",
     name = "38. Eastern Plaguelands 52-53 (Skyborne)",
-    version = 2,
+    version = 3,
     faction = "Horde",
     race = { "Skyborne" },
     minLevel = 52,
