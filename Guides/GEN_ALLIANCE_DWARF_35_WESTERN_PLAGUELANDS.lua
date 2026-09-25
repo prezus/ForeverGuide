b7e7d6d53e0fd967c084760d1e208560fd0c9884
prezus <prezus@gmail.com>
2026-09-25 15:20:13 -0600

    Guides: 35. Western Plaguelands 50-50 (Dwarf)

--- Guides/GEN_ALLIANCE_DWARF_35_WESTERN_PLAGUELANDS.lua
+++ Guides/GEN_ALLIANCE_DWARF_35_WESTERN_PLAGUELANDS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_DWARF_35_WESTERN_PLAGUELANDS",
     name = "35. Western Plaguelands 50-50 (Dwarf)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Dwarf", "Gnome" },
     minLevel = 50,
