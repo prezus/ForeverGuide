1bc32f72145ccc0d5d621f7266e453023357dcd1
prezus <prezus@gmail.com>
2026-09-25 15:20:24 -0600

    Guides: 14. Desolace 42-43 (Orc)

--- Guides/GEN_HORDE_ORC_14_DESOLACE.lua
+++ Guides/GEN_HORDE_ORC_14_DESOLACE.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_HORDE_ORC_14_DESOLACE",
     name = "14. Desolace 42-43 (Orc)",
-    version = 2,
+    version = 3,
     faction = "Horde",
     race = { "Orc", "Troll" },
     minLevel = 42,
