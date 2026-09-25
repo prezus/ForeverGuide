ce12f90e9be71b571f1c03e76eb5c7be4b09d73c
prezus <prezus@gmail.com>
2026-09-25 15:20:14 -0600

    Guides: 41. Eastern Plaguelands 52-53 (Dwarf)

--- Guides/GEN_ALLIANCE_DWARF_41_EASTERN_PLAGUELANDS.lua
+++ Guides/GEN_ALLIANCE_DWARF_41_EASTERN_PLAGUELANDS.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_DWARF_41_EASTERN_PLAGUELANDS",
     name = "41. Eastern Plaguelands 52-53 (Dwarf)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Dwarf", "Gnome" },
     minLevel = 52,
