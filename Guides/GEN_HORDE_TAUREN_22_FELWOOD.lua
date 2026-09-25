fab6dd713ac2f4fa98e5fbd5d882caf779ee7a40
prezus <prezus@gmail.com>
2026-09-25 15:20:32 -0600

    Guides: 22. Felwood 47-47 (Tauren)

--- Guides/GEN_HORDE_TAUREN_22_FELWOOD.lua
+++ Guides/GEN_HORDE_TAUREN_22_FELWOOD.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_HORDE_TAUREN_22_FELWOOD",
     name = "22. Felwood 47-47 (Tauren)",
-    version = 2,
+    version = 3,
     faction = "Horde",
     race = { "Tauren" },
     minLevel = 47,
