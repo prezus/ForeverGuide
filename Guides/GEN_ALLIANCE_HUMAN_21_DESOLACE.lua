66a727ba583525f874591fb2b629279da2a87e3b
prezus <prezus@gmail.com>
2026-09-25 15:20:16 -0600

    Guides: 21. Desolace 42-42 (Human)

--- Guides/GEN_ALLIANCE_HUMAN_21_DESOLACE.lua
+++ Guides/GEN_ALLIANCE_HUMAN_21_DESOLACE.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_ALLIANCE_HUMAN_21_DESOLACE",
     name = "21. Desolace 42-42 (Human)",
-    version = 2,
+    version = 3,
     faction = "Alliance",
     race = { "Human" },
     minLevel = 42,
