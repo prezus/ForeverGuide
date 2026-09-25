3549df224cf3006e5ef91c5d3bf2e0f2ce9a20d3
prezus <prezus@gmail.com>
2026-09-25 15:20:28 -0600

    Guides: 30. Orgrimmar 49-49 (Undead)

--- Guides/GEN_HORDE_SCOURGE_30_ORGRIMMAR.lua
+++ Guides/GEN_HORDE_SCOURGE_30_ORGRIMMAR.lua
@@ -3,7 +3,7 @@ local _, ns = ...
 ns.RegisterGuide({
     id = "GEN_HORDE_SCOURGE_30_ORGRIMMAR",
     name = "30. Orgrimmar 49-49 (Undead)",
-    version = 2,
+    version = 3,
     faction = "Horde",
     race = { "Scourge" },
     minLevel = 49,
